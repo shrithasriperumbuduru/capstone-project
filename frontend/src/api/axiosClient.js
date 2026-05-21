0ee281d081874a61a2e533313b17cff691c73e1b
Shritha <Shrithasriperumbuduru@gmail.com>
2026-05-21 12:07:47 +0530

    initial capstone upload

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blogapp-iyjr.onrender.com",
+baseURL: "http://localhost:5000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
